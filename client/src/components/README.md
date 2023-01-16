11d9e754d315f1e1e6150df19e3ce2911d500eb5
Monica Kumaran <persinammon@users.noreply.github.com>
2023-01-16 15:29:06 -0800

    Webpack succesfully works with React

---
+++ client/src/components/README.md
@@ -0,0 +1,5 @@
+### Temporary Notes
+
+This is a temporary README to keep track of project.
+
+These components were made for the MERN stack tutorial by MongoDB.
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,44 @@
-const path = require("path")
-const webpack = require("webpack")
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
 module.exports = {
-    entry: './src/index.js'
-}
+  mode: 'development',  
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ],
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  }
+};
