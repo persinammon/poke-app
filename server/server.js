8c0696537d1b196315da2fa0c995f5cdc84e200d
Monica Kumaran <persinammon@users.noreply.github.com>
2023-01-17 06:26:52 -0800

    Auth0 works

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8000;
 const dbo = require("./db/conn");
+const { requiresAuth } = require('express-openid-connect');
 
 
 // Start up an instance of app
@@ -14,16 +15,26 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
-
-app.use(express.static('client'));
+app.use(require("./routes/auth"));
 
 /* Routes */
 // Use let baseURL = to build requests to PokeAPI
 // Use const res = await fetch('url')
 
-app.get('/', function (req, res) {
+//send data through JSON for home page
+app.get('/home', function (req, res) {
     res.send('hello world');
   })
+
+//sends data for user if logged in
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+  
 
 const data = []
 app.post('/', function (req, res) {
@@ -31,7 +42,6 @@ app.post('/', function (req, res) {
     res.send('POST received')
 })
   
-
 // Create server
 const server = app.listen(port, ()=>{
     dbo.connectToServer(function (err) {
@@ -39,6 +49,5 @@ const server = app.listen(port, ()=>{
 
     });
     console.log("server running")
-    // Create as link to localhost port
     console.log(`running on localhost:${port}`)})
 
