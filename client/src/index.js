4bbea8a2cf7d4426fb595ac9cd868f71e5e3f3be
Monica Kumaran <persinammon@users.noreply.github.com>
2023-01-17 11:12:15 -0800

    Auth0 words with frontend; all that's left is securing individual endpoints https://auth0.com/docs/quickstart/spa/react/02-calling-an-api

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import './stylesheets/site-style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
+root.render(
+  <BrowserRouter>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}>
+    <App />
+  </Auth0Provider></BrowserRouter>);
