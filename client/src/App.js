bb05bee278238761caf6081096dba6306d427265
Monica Kumaran <persinammon@users.noreply.github.com>
2023-01-17 11:12:15 -0800

    Auth0 words with frontend; all that's left is securing individual endpoints https://auth0.com/docs/quickstart/spa/react/02-calling-an-api

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
 import Create from "./components/create";
+import LoginButton from "./components/login";
+import './stylesheets/site-style.scss';
+import LogoutButton from "./components/logout";
  
 const App = () => {
  return (
@@ -18,6 +21,8 @@ const App = () => {
        <Route path="/edit/:id" element={<Edit />} />
        <Route path="/create" element={<Create />} />
      </Routes>
+     <LoginButton />
+     <LogoutButton />
    </div>
  );
 };
