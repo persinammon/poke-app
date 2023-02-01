02e0cca1c1cb83ae9048975a0be70f41e690a86e
Monica Kumaran <persinammon@users.noreply.github.com>
2023-02-01 14:11:12 -0800

    Working on local mongo setup; in meantime creating endpoints backed by cloud db

--- mongosetup.js
+++ mongosetup.js
@@ -1,3 +1,12 @@
 //initialize with test data in database, runs as mongosh commands
 //later on can create a user and give it a lower privilege role, if relevant
-show dbs
+
+const collection1 = 'pokemon';
+const collection2 = 'trainers';
+const collection3 = 'teams';
+
+db.createCollection(collection1);
+db.createCollection(collection2);
+db.createCollection(collection3);
+
+
