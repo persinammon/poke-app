904066d1d10f35c1581fdef7026ab2127aef50cf
Monica Kumaran <persinammon@users.noreply.github.com>
2023-02-05 21:58:05 -0800

    Added more to the skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
  
 // We use Route in order to define the different routes of our application
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Redirect} from "react-router-dom";
 import AuthenticationButton from "./components/authentication-button";
  
 // We import all the components we need in our app
@@ -17,6 +17,7 @@ import PlayerPokemon from "./pages/player-pokemon";
 import PlayerTeam from "./pages/player-team";
 import ErrorPage from "./pages/error-page";
 import LandingPage from "./pages/landing-page";
+import ViewPokemon from "./pages/all-pokemon";
  
 const App = () => {
  return (
@@ -32,18 +33,18 @@ const App = () => {
         <Route path="mypokemon" element={<PlayerPokemon />} />
         <Route path="myteams" element={<PlayerTeam />} />
         <Route path="*" element={<ErrorPage />} />
-        <Route path="pokemon" element={<Invoices />}>
-          <Route path="all" element={} />
-          <Route path="all/:pageID" element={<Invoice />} />
-          <Route path=":pokemonID" element={<SentInvoices />} />
+        <Route path="pokemon" element={<Redirect to="/home" />}>
+          <Route path="all" element={<ViewPokemon />} />
+          <Route path="all/:pageID" element={<ViewPokemon />} />
+          <Route path=":pokemonID" element={<Pokemon />} />
         </Route>
-        <Route path="trainer" element={<Invoices />}>
-          <Route path=":trainerID" element={<Invoice />} />
-          <Route path=":trainerID/pokemon" element={<Invoice />} />
+        <Route path="trainer" element={<Redirect to="/home" />}>
+          <Route path=":trainerID" element={<Trainer />} />
+          <Route path=":trainerID/pokemon" element={<ViewPokemon />} />
         </Route>
-        <Route path="team" element={<Invoices />}>
-          <Route path=":teamID" element={} />
-          <Route path="create" element={<Invoice />} />
+        <Route path="team" element={<Redirect to="/home" />}>
+          <Route path=":teamID" element={<Team />} />
+          <Route path="create" element={<CreateTeam />} />
         </Route>
       </Routes>
      </div>
---
+++ client/src/pages/all-pokemon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+  
+const ViewPokemon = () => {
+  return (
+    <h1>This is the page to view pokemon, paginated.</h1>
+  );
+};
+  
+export default ViewPokemon;
